68b5707fa9eb5252f021a69292f7a81116289ee7
turkiea <turkiea@ensimag.fr>
2023-07-23 18:24:49 +0200

    add user_color to play with black pieces

--- alpha_beta.py
+++ alpha_beta.py
@@ -3,7 +3,7 @@ import board, piece_rate
 MAX_SCORE = 9999
 MIN_SCORE = -9999
 
-def alphabeta(chessboard, depth, alpha, beta, is_max):
+def alphabeta(chessboard, depth, alpha, beta, is_max, user_color):
     # Perform Alpha-Beta Pruning algorithm to evaluate the best move 
     if (depth == 0):
         return piece_rate.chessboard_evaluation(chessboard)
@@ -12,9 +12,9 @@ def alphabeta(chessboard, depth, alpha, beta, is_max):
         best_score = MIN_SCORE
         for action in chessboard.all_actions_on_list("W"):
             copy = board.Board.copy(chessboard)
-            copy.perform_action(action)
+            copy.perform_action(action, user_color)
 
-            best_score = max(best_score, alphabeta(copy, depth-1, alpha, beta, False))
+            best_score = max(best_score, alphabeta(copy, depth-1, alpha, beta, False, user_color))
             alpha = max(alpha, best_score)
             if (beta <= alpha):
                 break
@@ -23,9 +23,9 @@ def alphabeta(chessboard, depth, alpha, beta, is_max):
         best_score = MAX_SCORE
         for action in chessboard.all_actions_on_list("B"):
             copy = board.Board.copy(chessboard)
-            copy.perform_action(action)
+            copy.perform_action(action, user_color)
 
-            best_score = min(best_score, alphabeta(copy, depth-1, alpha, beta, True))
+            best_score = min(best_score, alphabeta(copy, depth-1, alpha, beta, True, user_color))
             beta = min(beta, best_score)
             if (beta <= alpha):
                 break
